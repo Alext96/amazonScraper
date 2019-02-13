3718b56a00cba1163a7407eac64193ac23013bc1
Alext96 <alextarberg96@gmail.com>
2019-02-13 10:08:17 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AmazonScraper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Scraper scraper;
+        public MainWindow()
+        {
+            InitializeComponent();
+            scraper = new Scraper();
+            DataContext = scraper;
+        }
+
+        private void BtnScraper_OnClick(object sender, RoutedEventArgs e)
+        {
+            scraper.ScrapeData(TBPage.Text);
+        }
+
+        private void ItemExport_OnClick(object sender, RoutedEventArgs e)
+        {
+            scraper.Export();
+        }
+    }
+}
---
+++ Scraper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using CsvHelper;
+using HtmlAgilityPack;
+
+namespace AmazonScraper
+{
+    public class Scraper
+    {
+        private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+        public ObservableCollection<EntryModel> Entries
+        {
+            get { return _entries; }
+            set { _entries = value; }
+        }
+
+
+        public void ScrapeData(string page)
+        {
+            var web = new HtmlWeb();
+            string html;
+            using (var wc = new GZipWebClient())
+                html = wc.DownloadString(page);
+
+            var htmldocObject = new HtmlDocument();
+            htmldocObject.LoadHtml(html);
+
+            //var doc = web.Load(page);
+
+            var Articles = htmldocObject.DocumentNode.SelectNodes("//*[@class = 's-result-item  celwidget  ']");
+
+            foreach (var articles in Articles)
+            {
+                var header = HttpUtility.HtmlDecode(articles.SelectSingleNode(".//div[@class = 'a-row a-spacing-mini']").InnerText);
+                var description = HttpUtility.HtmlDecode(articles
+                    .SelectSingleNode(".//div[@class = 'a-row a-spacing-none']")
+                    .InnerText);
+
+                Debug.Print($"Title: {header} \n Description {description}");
+                _entries.Add(new EntryModel {Title = header, Description = description});
+            }
+
+        }
+
+        public void Export()
+        {
+            using (TextWriter tw = File.CreateText("SampleData.csv"))
+            {
+                using (var cw = new CsvWriter(tw))
+                {
+                    foreach (var entry in Entries)
+                    {
+                        cw.WriteRecord(entry);
+                        cw.NextRecord();
+                    }
+                }
+            }
+        }
+    }
+}
